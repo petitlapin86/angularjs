458005828afb2e8e467256f2b3c53f6453741938
petitlapin86 <skinnerpa86@yahoo.com>
2019-09-18 17:42:46 -0400

    learn currency upper case and lower case angular

---
+++ currency/app.js
@@ -0,0 +1,26 @@
+angular.module('app', []).controller("MainController", function() {
+  
+  this.items = [
+      {
+        name: "Book",
+        price: "5.95"
+      },
+      {
+        name: "Mint",
+        price: ".15"
+      },
+      {
+        name: "Printer",
+        price: "240"
+      },
+      {
+        name: "Thing",
+        price: ""
+      },
+      {
+        name: "Wisdom",
+        price: "Priceless"
+      },
+  ];  
+  
+});
